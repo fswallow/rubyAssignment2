9d3cb4377bae7c2b972a70b1ba69024227a1e041
fswallow <b5015363@my.shu.ac.uk>
2019-04-19 12:29:00 +0100

    Users can register/create account, fully functioning log in/logout, add, edit and delete reviews

--- EPR_app/config/routes.rb
+++ EPR_app/config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :ipads
   resources :iwatches
   resources :profiles
+  resources :users
+  resources :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
