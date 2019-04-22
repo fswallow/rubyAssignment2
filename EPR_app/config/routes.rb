8a4f63eb131cef416a3e19dd40c79c417bd09450
fswallow <b5015363@my.shu.ac.uk>
2019-04-22 14:16:24 +0100

    Successully Implemented Custom Stylesheet and populated site with relevant information

--- EPR_app/config/routes.rb
+++ EPR_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'static_pages/home'
   root 'static_pages#home'
   get 'static_pages/about'
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   resources :profiles
   resources :users
   resources :sessions
+
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
